f578e3f937549044aa88b6874fd3a25637c8b2a3
Andrii Stadnyk <stadnyk.andy@gmail.com>
2024-06-21 01:49:39 +0300

    sending email

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -1,31 +1,72 @@
 import { NextRequest, NextResponse } from "next/server";
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEEND_API_KEY);
 
 export async function POST(req: NextRequest) {
-  const data = await req.json();
-  const email = data.email;
-
+  const { email } = await req.json();
   try {
-    const transporter = nodemailer.createTransport({
-      service: "google",
-      auth: {
-        user: process.env.NEXT_PUBLIC_EMAIL,
-        pass: process.env.NEXT_PUBLIC_PASSWORD,
-      },
-    });
-
-    const mailOption = {
-      from: `Cheralds <${process.env.NEXT_PUBLIC_EMAIL}>`,
+    const { error } = await resend.emails.send({
+      from: "Cheralds <onboarding@resend.dev>",
       to: "stadnyk.andy@gmail.com",
-      subject: "Hello New Email",
-      text: "Hello world?",
-      html: `<h1>${email}...?</h1>`,
-    };
-
-    const info = await transporter.sendMail(mailOption);
-
-    return NextResponse.json({ info, email });
+      subject: 'New "Cheralds" subscriber',
+      html: `Subscriber's email: <strong>${email}</strong>`,
+    });
+    if (error) {
+      return NextResponse.json({ error }, { status: 500 });
+    }
+    return NextResponse.json({
+      message: "Email send successfully",
+    });
   } catch (err) {
-    return NextResponse.json({ mesage: "error" }, { status: 500 });
+    return NextResponse.json({ message: err }, { status: 500 });
   }
 }
+
+// import nodemailer from "nodemailer";
+// import Mail from "nodemailer/lib/mailer";
+
+// const transporter = nodemailer.createTransport({
+//   service: "zoho",
+//   host: "smtpro.zoho.in",
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: process.env.NEXT_PUBLIC_EMAIL,
+//     pass: process.env.NEXT_PUBLIC_PASSWORD,
+//   },
+// });
+
+// export async function POST(req: NextRequest) {
+//   const { email } = await req.json();
+
+//   const mailOption: Mail.Options = {
+//     from: `Cheralds <${process.env.NEXT_PUBLIC_EMAIL}>`,
+//     to: "stadnyk.andy@gmail.com",
+//     subject: "Subject text",
+//     html: `<h1>${email}...?</h1>`,
+//     text: "Text text",
+//   };
+
+//   const sendMailPromise = () =>
+//     new Promise<string>((resolve, reject) => {
+//       transporter.sendMail(mailOption, function (err) {
+//         console.log({ err });
+
+//         if (!err) {
+//           resolve("Email sent successfully");
+//         } else {
+//           reject(err.message);
+//         }
+//       });
+//     });
+
+//   try {
+//     await sendMailPromise();
+//     await transporter.sendMail(mailOption);
+
+//     return Response.json("data");
+//   } catch (error) {
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
--- src/components/Footer/SubscribeForm.tsx
+++ src/components/Footer/SubscribeForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-// import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -17,31 +17,39 @@ const formSchema = z.object({
 type Form = z.infer<typeof formSchema>;
 
 export function SubscribeForm() {
-  // const router = useRouter();
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const form = useForm<Form>({
     resolver: zodResolver(formSchema),
     defaultValues: getDefaults(formSchema),
   });
 
-  // const subscribe = useMutation({
-  //   mutationFn: (data: Form) => console.log(data),
-  //   onSuccess: (data) => {
-  //     router.refresh();
-  //   },
-  // });
+  async function onSubmit(data: Form) {
+    setSuccess("");
+    setError("");
+    setLoading(true);
 
-  function onSubmit(data: Form) {
-    fetch("api/subscribe", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
+    try {
+      const res = await fetch("api/subscribe", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const { message } = await res.json();
+      setSuccess(message);
+      setLoading(false);
+
+      setTimeout(() => setSuccess(""), 4000);
+    } catch (err) {
+      setLoading(false);
+      if (err instanceof Error) {
+        setError(err?.message);
+      }
+    }
   }
 
   return (
@@ -52,16 +60,16 @@ export function SubscribeForm() {
           "flex w-full flex-col justify-end gap-x-6 gap-y-4 sm:flex-row sm:items-center"
         }
       >
-        <FormTextInput<Form>
-          fieldName={"email"}
-          placeholder={"Enter your Email"}
-          className={{ wrapper: "flex-1 md:max-w-[390px]" }}
-        />
-        <Button
-          type={"submit"}
-          colorVariant={"white"}
-          // loading={subscribe.isLoading}
-        >
+        <div className={"flex-1 md:max-w-[390px]"}>
+          <FormTextInput<Form>
+            fieldName={"email"}
+            placeholder={"Enter your Email"}
+            className={{ wrapper: "mb-2" }}
+          />
+          {success && <p className={"text-green_ch-400"}>{success}</p>}
+          {error && <p className={"text-red-500"}>{error}</p>}
+        </div>
+        <Button type={"submit"} colorVariant={"white"} loading={loading}>
           Subscribe
         </Button>
       </form>
