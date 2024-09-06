b30fce5ba78150cb1ffa3e43d411f898f75edb4e
polina-777 <80350878+polina-777@users.noreply.github.com>
2024-09-06 17:24:38 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,8 +26,8 @@ export default async function Home() {
       <DogPow />
       <Reviews />
       <HowToGetCherryTree />
-/*      <Hand />
+     <Hand />
       {/* <Video /> */}
-    </> */
+    </> 
   );
 }
